f27d23fb56ce8968441428b0d8a085d2282aec67
Zoltan Horvath <zoltan.horvath@proxybit.hu>
2022-01-15 15:07:17 +0100

    small bugfix in view separation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,14 +14,7 @@
             <div class="row">
                 <div class="col-lg-12">
                     <div class="card">
-                        <div class="card-header border-0">
-                            <div class="d-flex justify-content-between">
-                                <h3 class="card-title">Online Store Visitors</h3>
-                            </div>
-                        </div>
-                        <div class="card-body">
                             @yield('content')
-                        </div>
                     </div>
                     <!-- /.card -->
                 </div>
--- resources/views/main-page/main-page.blade.php
+++ resources/views/main-page/main-page.blade.php
@@ -1,6 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-    This is the main page
+    <div class="card-header border-0">
+        <div class="d-flex justify-content-between">
+            <h3 class="card-title">Online Store Visitors</h3>
+        </div>
+    </div>
+    <div class="card-body">
+        <div>
+            <h2>Greetings!</h2>
+        </div>
+    </div>
 @endsection
 
